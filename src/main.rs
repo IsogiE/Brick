a1742eaacff446aea06d284cc009564e80aa03de
IsogiE <88090493+IsogiE@users.noreply.github.com>
2026-09-04 22:37:08 +0200

    Restore default renderer

--- src/main.rs
+++ src/main.rs
@@ -55,10 +55,6 @@ fn main() -> Result<(), eframe::Error> {
 
     let options = eframe::NativeOptions {
         viewport,
-        #[cfg(target_os = "windows")]
-        renderer: eframe::Renderer::Wgpu,
-        #[cfg(not(target_os = "windows"))]
-        renderer: eframe::Renderer::Glow,
         ..Default::default()
     };
 
